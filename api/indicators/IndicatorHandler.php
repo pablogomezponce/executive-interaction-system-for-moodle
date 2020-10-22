b5242391461c9e7adc562d06015261d9aaf2ceca
Pablo Gómez Ponce <pablo.gp@students.salle.url.edu>
2020-10-22 17:46:56 +0200

    Offer kind of information on extraction response

--- api/indicators/IndicatorHandler.php
+++ api/indicators/IndicatorHandler.php
@@ -21,15 +21,15 @@ class IndicatorHandler
         $this->sql = new \PDO('mysql:host=' . $dbSettings['address'] . ';dbname=' . $dbSettings['dbname'], $dbSettings['userNameDB'], $dbSettings['passwordDB']);
     }
 
-    private static $individualTotalInteractions = 'SELECT firstname, count(action) as amount FROM mdl_logstore_standard_log mlsl JOIN mdl_user mu ON mu.id = mlsl.userid WHERE courseid = %courseid AND userid = %userid group by userid';
-    private static $individualRankingInteractions = 'select ranking.userid, ranking.times, ranking.position, mu.username  from mdl_user mu join ( SELECT userid, count(action) as "times", row_number() over (order by count(action) desc) position FROM mdl_logstore_standard_log mlsl where courseid = %courseid group by userid order by count(action) desc ) ranking on mu.id = ranking.userid where userid = %userid';
-    private static $individualAVGTimeToModify = 'select userid, sec_to_time(avg(time_to_sec(timediff(from_unixtime(ma.duedate), from_unixtime(mas.timemodified))))) as avg from mdl_assign ma     JOIN mdl_assign_submission mas on mas.assignment = ma.id where course = %courseid   and ma.duedate - mas.timemodified > 0   and status = "submitted" and userid = %userid group by userid;';
-    private static $individualAmountOfNP = 'select userid, (select count(id) from mdl_assign where course = %courseid) - count(distinct assignment) as NP from mdl_assign_submission mas where mas.assignment in (select ma2.id from mdl_assign ma2 where course = %courseid) and userid = %userid group by userid;';
+    private static $individualTotalInteractions = ['sql' => 'SELECT firstname, count(action) as amount FROM mdl_logstore_standard_log mlsl JOIN mdl_user mu ON mu.id = mlsl.userid WHERE courseid = %courseid AND userid = %userid group by userid', 'kind' => "COUNTER"];
+    private static $individualRankingInteractions =  ['sql' => 'select ranking.userid, ranking.times, ranking.position as amount, mu.username  from mdl_user mu join ( SELECT userid, count(action) as "times", row_number() over (order by count(action) desc) position FROM mdl_logstore_standard_log mlsl where courseid = %courseid group by userid order by count(action) desc ) ranking on mu.id = ranking.userid where userid = %userid', 'kind' => 'COUNTER'];
+    private static $individualAVGTimeToModify =  ['sql' => 'select userid, sec_to_time(avg(time_to_sec(timediff(from_unixtime(ma.duedate), from_unixtime(mas.timemodified))))) as amount from mdl_assign ma JOIN mdl_assign_submission mas on mas.assignment = ma.id where course = %courseid   and ma.duedate - mas.timemodified > 0   and status = "submitted" and userid = %userid group by userid;', 'kind' => 'COUNTER'];
+    private static $individualAmountOfNP =  ['sql' => 'select userid, (select count(id) from mdl_assign where course = %courseid) - count(distinct assignment) as amount from mdl_assign_submission mas where mas.assignment in (select ma2.id from mdl_assign ma2 where course = %courseid) and userid = %userid group by userid;', 'kind' => 'COUNTER'];
 
-    private static $grupalTotalInteractions = 'SELECT firstname, count(action) as amount FROM mdl_logstore_standard_log mlsl JOIN mdl_user mu ON mu.id = mlsl.userid WHERE courseid = %courseid group by userid';
-    private static $grupalRankgingInteractions = 'select ranking.userid, ranking.times, ranking.position, mu.username  from mdl_user mu join ( SELECT userid, count(action) as "times", row_number() over (order by count(action) desc) position FROM mdl_logstore_standard_log mlsl where courseid = %courseid group by userid order by count(action) desc ) ranking on mu.id = ranking.userid';
-    private static $grupalAVGTimeToModify = 'select userid, sec_to_time(avg(time_to_sec(timediff(from_unixtime(ma.duedate), from_unixtime(mas.timemodified))))) as avg from mdl_assign ma     JOIN mdl_assign_submission mas on mas.assignment = ma.id where course = %courseid   and ma.duedate - mas.timemodified > 0   and status = "submitted" group by userid;';
-    private static $grupalAmountOfNP = 'select userid, (select count(id) from mdl_assign where course = %courseid) - count(distinct assignment) as NP from mdl_assign_submission mas where mas.assignment in (select ma2.id from mdl_assign ma2 where course = %courseid) group by userid;';
+    private static $grupalTotalInteractions =  ['sql' => 'SELECT firstname, count(action) as amount FROM mdl_logstore_standard_log mlsl JOIN mdl_user mu ON mu.id = mlsl.userid WHERE courseid = %courseid group by userid', 'kind' => 'LIST'];
+    private static $grupalRankgingInteractions =  ['sql' => 'select ranking.userid, ranking.times, ranking.position, mu.username  from mdl_user mu join ( SELECT userid, count(action) as "times", row_number() over (order by count(action) desc) position FROM mdl_logstore_standard_log mlsl where courseid = %courseid group by userid order by count(action) desc ) ranking on mu.id = ranking.userid','kind' => 'LIST'];
+    private static $grupalAVGTimeToModify =  ['sql' => 'select userid, sec_to_time(avg(time_to_sec(timediff(from_unixtime(ma.duedate), from_unixtime(mas.timemodified))))) as avg from mdl_assign ma     JOIN mdl_assign_submission mas on mas.assignment = ma.id where course = %courseid   and ma.duedate - mas.timemodified > 0   and status = "submitted" group by userid;', 'kind' => 'LIST'];
+    private static $grupalAmountOfNP =  ['sql' => 'select userid, (select count(id) from mdl_assign where course = %courseid) - count(distinct assignment) as NP from mdl_assign_submission mas where mas.assignment in (select ma2.id from mdl_assign ma2 where course = %courseid) group by userid;', 'kind' => 'LIST'];
 
     public static $gruposInteracciones = [
         'individual' => 'Individuales',
@@ -76,7 +76,7 @@ class IndicatorHandler
 
 
         if (is_array($array)) {
-            if (isset($array[$keySearch])) {
+            if (isset($array[$keySearch]['sql'])) {
                 $ans = $array;
             }
 
@@ -98,18 +98,21 @@ class IndicatorHandler
         foreach ($queries_array as $item) {
             $query = $this->findKey($allIndicators, $item['name'])[$item['name']];
 
+            $sql = $query['sql'];
 
-            $query = str_replace('%courseid', $courseid, $query);
-            $query = str_replace('%userid', $_SESSION['user']['id'], $query);
 
-            $stmt = $this->sql->prepare($query);
+            $sql = str_replace('%courseid', $courseid, $sql);
+            $sql = str_replace('%userid', $_SESSION['user']['id'], $sql);
+
+            $stmt = $this->sql->prepare($sql);
 
             //$stmt->bindParam('%courseid', $courseid);
             //$stmt->bindParam('%courseid', $_SESSION['user']['id']);
             $stmt->execute();
             $resultset = $stmt->fetchAll();
 
-            $responseArray[$item['name']] = $resultset;
+            $responseArray[$item['name']] = ['content'=>$resultset, 'kind' => $query['kind']];
+
 
         }
 
