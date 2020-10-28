fe671e2ed6d83d7f9a201e42e02598a867d8826a
Pablo Gómez Ponce <pablo.gp@students.salle.url.edu>
2020-10-28 10:10:47 +0100

    New indicators

--- api/indicators/IndicatorHandler.php
+++ api/indicators/IndicatorHandler.php
@@ -28,8 +28,7 @@ class IndicatorHandler
     private static $individualResourcesSeen = ['sql' => "select mr.name as 'Nombre del recurso', count(mlsl.id) as 'Veces visto' from mdl_logstore_standard_log mlsl     join mdl_resource mr on mr.id = mlsl.objectid     join mdl_user mu on mu.id = mlsl.userid where courseid = %courseid and component = 'mod_resource' and mlsl.userid = %userid group by userid, name order by name asc;", 'kind' => 'LIST' ];
     private static $individualAmountResourcesSeen = ['sql' => "select count(mlsl.id) as 'amount' from mdl_logstore_standard_log mlsl     join mdl_resource mr on mr.id = mlsl.objectid     join mdl_user mu on mu.id = mlsl.userid where courseid = %courseid and component = 'mod_resource' and mlsl.userid = %userid group by userid;", 'kind' => 'COUNTER'];
     private static $individualCourseTimesSeenAmount = ['sql' => "select rank() over (order by count(mlsl.id) desc) as '#' ,mu.username as 'Nombre de usuario', count(mlsl.id) as 'amount' from mdl_logstore_standard_log mlsl join mdl_user mu on mu.id = mlsl.userid where courseid = %courseid and mlsl.eventname = '\\\\core\\\\event\\\\course_viewed' and mu.id = %userid group by mu.username order by count(mlsl.id) desc" , 'kind' => 'COUNTER'];
-
-
+    private static $individualURLSeen = ['sql' => "SELECT rank() over (order by count(mlsl.id) desc) as '#', name as 'Nombre del enlace', count(mlsl.id) as 'Cantidad de veces visto' FROM mdl_url murl     join mdl_logstore_standard_log mlsl on mlsl.courseid = murl.course and component = 'mod_url' and murl.id = mlsl.objectid     join mdl_user mu on mu.id = mlsl.userid WHERE course = %courseid and action = 'viewed' and userid = %userid group by name;", 'kind' => 'LIST'];
 
     private static $grupalRankgingInteractions =  ['sql' => 'select ranking.position as "#",mu.username as "Nombre de usuario", ranking.times as "Cantidad de interacciones" from mdl_user mu join ( SELECT userid, count(action) as "times", row_number() over (order by count(action) desc) position FROM mdl_logstore_standard_log mlsl where courseid = %courseid group by userid order by count(action) desc ) ranking on mu.id = ranking.userid','kind' => 'LIST'];
     private static $grupalAVGTimeToModify =  ['sql' => 'select mu.username as "Nombre de usuario",        sec_to_time(avg(time_to_sec(timediff(from_unixtime(ma.duedate), from_unixtime(mas.timemodified))))) as "Tiempo de margen promedio"  from mdl_assign ma          JOIN mdl_assign_submission mas on mas.assignment = ma.id      JOIN mdl_user mu on mu.id = mas.userid where course = %courseid      and ma.duedate - mas.timemodified > 0      and status = "submitted"  group by userid;', 'kind' => 'LIST'];
@@ -37,6 +36,8 @@ class IndicatorHandler
     private static $grupalResourcesSeenByStudent = ['sql' => "select mu.username as 'Nombre de usuario', mr.name as 'Nombre del recurso', count(mlsl.id) as 'Veces visto' from mdl_logstore_standard_log mlsl     join mdl_resource mr on mr.id = mlsl.objectid     join mdl_user mu on mu.id = mlsl.userid where courseid = %courseid and component = 'mod_resource' group by userid, name order by userid, name asc;", 'kind' => 'LIST'];
     private static $grupalResourcesSeenTotal = ['sql' => "select row_number() over (order by count(mlsl.id) desc) as '#', mr.name as 'Nombre del recurso', count(mlsl.id) as 'Veces visto' from mdl_logstore_standard_log mlsl     join mdl_resource mr on mr.id = mlsl.objectid     join mdl_user mu on mu.id = mlsl.userid where courseid = %courseid and component = 'mod_resource' group by name order by count(mlsl.id) desc; ", 'kind'=>'LIST'];
     private static $grupalCourseTimesSeen = ['sql' => "select rank() over (order by count(mlsl.id) desc) as '#' ,mu.username as 'Nombre de usuario', count(mlsl.id) as 'Cantidad de accesos al curso' from mdl_logstore_standard_log mlsl join mdl_user mu on mu.id = mlsl.userid where courseid = %courseid and mlsl.eventname = '\\\\core\\\\event\\\\course_viewed' group by mu.username order by count(mlsl.id) desc" , 'kind' => 'LIST'];
+    private static $grupalURLseen = ['sql' => "SELECT rank() over (order by count(mlsl.id) desc) as '#', name as 'Nombre del enlace', count(mlsl.id) as 'Cantidad de veces visto' FROM mdl_url murl     join mdl_logstore_standard_log mlsl on mlsl.courseid = murl.course and component = 'mod_url' and murl.id = mlsl.objectid     join mdl_user mu on mu.id = mlsl.userid WHERE course = %courseid and action = 'viewed' group by name;", 'kind' => 'LIST'];
+    private static $grupalURLSeenPerUser = ['sql' => "SELECT mu.username as 'Nombre de usuario' ,name as 'Nombre del enlace', count(mlsl.id) as 'Cantidad de veces visto' FROM mdl_url murl     join mdl_logstore_standard_log mlsl on mlsl.courseid = murl.course and component = 'mod_url' and murl.id = mlsl.objectid     join mdl_user mu on mu.id = mlsl.userid WHERE course = %courseid and action = 'viewed' group by username, name;", 'kind' => 'LIST'];
 
     public static $gruposInteracciones = [
         'individual' => 'Individuales',
@@ -54,6 +55,8 @@ class IndicatorHandler
         $indicators['Glosario de recursos vistos'] = self::$individualResourcesSeen;
         $indicators['Cantidad de recursos vistos'] = self::$individualAmountResourcesSeen;
         $indicators['Cantidad de veces que has visto el curso']= self::$individualCourseTimesSeenAmount;
+        $indicators['URLs visitadas'] = self::$individualURLSeen;
+
         return $indicators;
     }
 
@@ -67,7 +70,9 @@ class IndicatorHandler
         $indicators['Lista de recursos vistos por alumno'] = self::$grupalResourcesSeenByStudent;
         $indicators['Ranking de recursos más vistos'] = self::$grupalResourcesSeenTotal;
         $indicators['Cantidad de accesos al curso'] = self::$grupalCourseTimesSeen;
-
+        $indicators['Acceso a las URL del curso'] = self::$grupalURLseen;
+        $indicators['Visitas a las URL por usuario'] = self::$grupalURLSeenPerUser;
+        
         return $indicators;
     }
 
